28fafc8615c15a49469e71b7b734b6602401a60e
Dimitris Mermigkas <dmermigkas@gmail.com>
2016-03-19 17:08:10 +0200

    Finished testing/coming-soon.html

--- testing/myscript.js
+++ testing/myscript.js
@@ -1,3 +1,6 @@
+
+(function(){
+
 $(window).scroll( function(){
 
   if ($(".navbar").offset().top > 50) {
@@ -6,3 +9,19 @@ $(window).scroll( function(){
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
+
+$('a.page-scroll').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1500);
+        event.preventDefault();
+    });
+
+$('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+
+})();
